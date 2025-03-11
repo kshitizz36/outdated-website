df5c32da4d2da61d65652f32c55f6477890d4a9a
Kshitiz <kshitizyadav69@gmail.com>
2025-03-12 00:37:47 +0530

    Automated commit message.

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
-import "@/styles/globals.css";
+import Head from 'next/head'; import '../styles/globals.css';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+import { AppProps } from 'next/app';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <>
+    <Head>
+      <title>My App</title>
+    </Head>
+    <Component {...pageProps} />
+  </>;
 }
+
+export default MyApp;
