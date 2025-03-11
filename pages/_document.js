9bffa1fa602eb1ee494ae48ff3c2536157a86d63
Kshitiz <kshitizyadav69@gmail.com>
2025-03-11 19:26:48 +0530

    Automated commit message.

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,37 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, { Head, Main, Title, Meta, Link, Script, NextScript } from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body className="antialiased">
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+
+export default class Document extends Document {
+  static async getInitialProps(ctx) {
+    return await Document.getInitialProps(ctx);
+  };
+
+
+  render() {
+    return (
+      <html lang={'en'}
+      >
+        <head
+        />
+        <body
+          className={'antialiased'}
+        >
+          <Main />
+          <div>
+            <Title></Title>
+            <Meta charSet={'utf-8'} />
+            <Meta name={'viewport'} content={'width=device-width, initial-scale=1'} />
+            <Link rel={'icon'} href={'/favicon.ico'} />
+            <title>Home</title>
+          </div>
+          <div>
+            <script>
+              console.log('hello world');
+            </script>
+          </div>
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
 }
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,4 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
+import { NextResponse } from 'next';
+export default async function handler(req, res) {
+  return NextResponse.json({ name: 'John Doe' }, { status: 200 });
 }
