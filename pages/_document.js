741020f0cf0bce72d46ad603ac35234499f73038
Kshitiz <kshitizyadav69@gmail.com>
2025-03-11 19:09:36 +0530

    Automated code updates by DependaPou

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,22 @@
-import { Html, Head, Main, NextScript } from "next/document";
+# This is a NextJS document file, let's create a new file called Document.js
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body className="antialiased">
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+class CustomDocument extends Document {
+  static getInitialProps(ctx) {
+    return nextDocument.getInitialProps(ctx);
+  }
+
+  render() {
+    return (
+      <Html lang='en'>
+        <Head />
+        <body className='antialiased'>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default CustomDocument;
