90540ec9f5f8bb1b50b7e3eda3195e4f938398e8
Kshitiz <kshitizyadav69@gmail.com>
2025-03-12 15:30:24 +0530

    Automated commit message.

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
-import "@/styles/globals.css";
+import { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
+
+// The original code used to import '@/styles/globals.css'; globally, but this is now handled as part of the Next.js configuration.
