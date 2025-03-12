dad3c3b64eaeda8abe89dcb408bc86ac9d911df8
Kshitiz <kshitizyadav69@gmail.com>
2025-03-12 15:32:12 +0530

    Automated commit message.

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
-import "@/styles/globals.css";
+import { AppProps } from 'next/app';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+// Next.js App
+export default function MyApp({ Component, pageProps }: AppProps) {
+	return <Component {...pageProps} />;
 }
