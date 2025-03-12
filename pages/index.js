3825107892cf3052e9d3214637471fe8fc0cf0ca
Kshitiz <kshitizyadav69@gmail.com>
2025-03-12 22:42:27 +0530

    Automated commit message.

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,19 @@
-// pages/index.js (Outdated Example)
+import React from 'react'; import Head from 'next/head'; import axios from 'axios';
 
-import React from 'react';
-import Head from 'next/head';
+export default function Home ({ posts }) {
+  return (<>
+    <Head>
+      <title>Next.js Example with Updated API</title>
+    </Head>
+    <h1>Next.js Example with Updated API</h1>
+    <ul>{posts.map((post) => (<li key={post.id}>{post.title}</li>))}</ul>
 
-class Home extends React.Component {
-  // getInitialProps is now outdated, replaced by getServerSideProps/getStaticProps/etc.
-  static async getInitialProps(context) {
-    // Fetch some data on the server
-    const data = await fetch('https://api.example.com/posts').then((res) =>
-      res.json()
-    );
-
-    return {
-      posts: data,
-    };
-  }
-
-  render() {
-    const { posts } = this.props;
-
-    return (
-      <div>
-        <Head>
-          <title>Outdated Next.js Example</title>
-        </Head>
-        <h1>Outdated Next.js Example</h1>
-        <ul>
-          {posts.map((post) => (
-            <li key={post.id}>{post.title}</li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
+  </>);
 }
 
-export default Home;
+export async function getServerSideProps({ req, res }){
+  const data = await axios.get('https://api.example.com/posts');
+  return {
+    props: data.data,
+  };
+}
