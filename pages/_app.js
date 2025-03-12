ee860193fd994bd9095287ce4575a2b93ebac08d
Kshitiz <kshitizyadav69@gmail.com>
2025-03-12 15:49:06 +0530

    Automated commit message.

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
-import "@/styles/globals.css";
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import '@styles/globals.css';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const meta = Component.getInitialProps ? Component.getInitialProps(pageProps) : null;
+
+  return (
+    <> 
+      <Head>{meta && <title>{meta.title}</title> ? meta.title : ''} </Head>
+      <Component {...pageProps} /> 
+    </>
+  );
+};
+
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,31 @@
-// pages/index.js (Outdated Example)
-
-import React from 'react';
+import { useState, useEffect } from 'react';
 import Head from 'next/head';
 
-class Home extends React.Component {
-  // getInitialProps is now outdated, replaced by getServerSideProps/getStaticProps/etc.
-  static async getInitialProps(context) {
-    // Fetch some data on the server
-    const data = await fetch('https://api.example.com/posts').then((res) =>
-      res.json()
-    );
+interface IPost {
+  id: number;
+  title: string;
+}
 
-    return {
-      posts: data,
+function Home({ posts }: { posts: IPost[] }) {
+  const [data, setData] = useState<IPost[]>([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('https://api.example.com/posts');
+      const { posts: data } = await response.json();
+      setData(data);
     };
-  }
+    fetchData();
+  }, []);
 
-  render() {
-    const { posts } = this.props;
-
-    return (
-      <div>
-        <Head>
-          <title>Outdated Next.js Example</title>
-        </Head>
-        <h1>Outdated Next.js Example</h1>
-        <ul>
-          {posts.map((post) => (
-            <li key={post.id}>{post.title}</li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Head><title>Outdated Next.js Example</title></Head>
+      <h1>Outdated Next.js Example</h1>
+      <ul>{data.map((post) => (
+        <li key={post.id}>{post.title}</li>
+      ))}</ul>
+    </div>
+  );
 }
 
 export default Home;
