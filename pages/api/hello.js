45c78bc94c8452ec9473efd5efc0067701b55cfd
Kshitiz <kshitizyadav69@gmail.com>
2025-03-13 23:53:43 +0530

    Automated commit message.

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,10 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { NextApiRequest, NextApiResponse } from 'next';
 
-export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    res.status(200).json({ name: 'John Doe' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,29 @@
-// pages/index.js (Outdated Example)
-
 import React from 'react';
-import Head from 'next/head';
+import { GetServerSideProps } from 'next/page';
 
-class Home extends React.Component {
-  // getInitialProps is now outdated, replaced by getServerSideProps/getStaticProps/etc.
-  static async getInitialProps(context) {
-    // Fetch some data on the server
-    const data = await fetch('https://api.example.com/posts').then((res) =>
-      res.json()
-    );
-
-    return {
-      posts: data,
-    };
-  }
-
-  render() {
-    const { posts } = this.props;
-
-    return (
-      <div>
-        <Head>
-          <title>Outdated Next.js Example</title>
-        </Head>
-        <h1>Outdated Next.js Example</h1>
-        <ul>
-          {posts.map((post) => (
-            <li key={post.id}>{post.title}</li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
+function Home({ posts }: { posts: any[] }) {
+  return (
+    <div>
+      <h1>Outdated Next.js Example</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
+
+type LandingPageProps = {
+  posts: any[];
+};
+
+export const getServerSideProps: GetServerSideProps<LandingPageProps> = async () => {
+  const res = await fetch('https://api.example.com/posts');
+  const data = await res.json();
+  return {
+    props: { posts: data }
+  };
+};
 
 export default Home;
