742f2a5a19959a9413298b9ee7e538f2c397e7e1
Kshitiz <kshitizyadav69@gmail.com>
2025-03-14 00:28:58 +0530

    Automated commit message.

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,30 @@
-// pages/index.js (Outdated Example)
-
-import React from 'react';
-import Head from 'next/head';
+import React from 'react'; import Head from 'next/head'; import axios from 'axios';
 
 class Home extends React.Component {
-  // getInitialProps is now outdated, replaced by getServerSideProps/getStaticProps/etc.
-  static async getInitialProps(context) {
-    // Fetch some data on the server
-    const data = await fetch('https://api.example.com/posts').then((res) =>
-      res.json()
-    );
-
-    return {
-      posts: data,
-    };
+  static async getServerSideProps(context) {
+    try {
+      const posts = await axios.get('https://api.example.com/posts');
+      return {
+        props: {
+          posts: posts.data,
+        },
+      };
+    } catch (err) {
+      console.error(err);
+      return {
+        props: {},
+      };
+    }
   }
 
   render() {
     const { posts } = this.props;
-
     return (
       <div>
         <Head>
-          <title>Outdated Next.js Example</title>
+          <title>Updated Next.js Example</title>
         </Head>
-        <h1>Outdated Next.js Example</h1>
+        <h1>Updated Next.js Example</h1>
         <ul>
           {posts.map((post) => (
             <li key={post.id}>{post.title}</li>
@@ -35,4 +35,27 @@ class Home extends React.Component {
   }
 }
 
-export default Home;
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasError: false,
+    };
+  }
+
+  componentDidCatch(error, info) {
+    this.setState({ hasError: true });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <div>Something went wrong.</div>;
+    }
+    return this.props.children;
+  }
+}
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+export default MyApp;
