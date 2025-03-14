3c89b7a6362519e51de986e6aea4c0a6c6c3392f
Kshitiz <kshitizyadav69@gmail.com>
2025-03-14 22:22:38 +0530

    Automated commit message.

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,19 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body className="antialiased">
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+interface DocumentProps {
+ _children: React.ReactNode;
+  __NEXT_32_EXPORT__?: never;
+  __NEXT_DATA__?: never;
+  __NEXT_REDUCED_COMPONENTS__?: never;
 }
+
+const Document = ({ children, ...props }: DocumentProps) => {
+  return (
+    <html lang='en'>
+      <head />
+      <body className='antialiased'>{children}</body>
+    </html>
+  )
+};
+
+export default Document;
