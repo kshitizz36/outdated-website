b29f32502a4b980a15636286547f0b109d8f6697
Kshitiz <kshitizyadav69@gmail.com>
2025-03-14 23:29:12 +0530

    Automated commit message.

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,17 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body className="antialiased">
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+export default class Document extends Document {
+  render() {
+    return (
+      <Html lang='en'>
+        <Head />;
+        <body className='dark'>{/* antialiased is not a suitable dark mode className, use dark instead */}
+          <Main />;
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+// Also consider removing the unused export 'Head' and 'Main'.
--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,37 @@
-// pages/index.js (Outdated Example)
+import { useState, useEffect } from 'react';
 
-import React from 'react';
-import Head from 'next/head';
-
-class Home extends React.Component {
-  // getInitialProps is now outdated, replaced by getServerSideProps/getStaticProps/etc.
-  static async getInitialProps(context) {
-    // Fetch some data on the server
-    const data = await fetch('https://api.example.com/posts').then((res) =>
-      res.json()
-    );
-
-    return {
-      posts: data,
+function Home({ posts }) {
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch('https://api.example.com/posts');
+      const data = await response.json();
+      return data;
     };
-  }
+    fetchPosts().then((data) => {
+      console.log(data);
+    });
+  }, []);
 
-  render() {
-    const { posts } = this.props;
+  return (
+    <div>
+      <title>Outdated Next.js Example</title>
+      <h1>Outdated Next.js Example</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 
-    return (
-      <div>
-        <Head>
-          <title>Outdated Next.js Example</title>
-        </Head>
-        <h1>Outdated Next.js Example</h1>
-        <ul>
-          {posts.map((post) => (
-            <li key={post.id}>{post.title}</li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
+export function getStaticProps() {
+  const data = await fetch('https://api.example.com/posts').then((res) => res.json());
+  return {
+    props: {
+      posts: data,
+    },
+  };
 }
 
 export default Home;
