fac82436b053d2b3df3392c01667e5df52ba45c1
Kshitiz <kshitizyadav69@gmail.com>
2025-03-29 17:14:51 +0530

    Automated commit message.

--- pages/_app.js
+++ pages/_app.js
@@ -1,1 +1,10 @@
-string
+import { AppProps } from 'next/app';
+import meta from '../components/Layout/Meta';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <><meta />
+  <Component {...pageProps} />
+  </>;
+}
+
+export default MyApp;
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,3 +1,10 @@
-export default function handler(req, res) {
-  return res.status(200).json({ name: "John Doe" });
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    res.status(200).json({ name: 'John Doe' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
 }
