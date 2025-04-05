5f10b293d74bbf220d012c23a2bd676ebaf0fd4c
Kshitiz <kshitizyadav69@gmail.com>
2025-04-05 12:36:32 +0530

    Automated commit message.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import React from 'react';
+// Remove import statement by importing styles in a more modern way using the next/config module
+import { appDir, publicUrlWithHash, publicUrl } from 'next/config';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+// If you still need to dynamically import components, use getStaticProps. Here is an updated version of your file using this approach.
+import Head from 'next/head';  // Next.js needs to update React version to dynamically import on getStaticProps
+
+export default function App({ Component, pageProps }) {
+  const { appDir, publicUrlWithHash, publicUrl } = require('next/config').default();
+
+  return (
+    <>
+      <Head>
+        <link rel="stylesheet" href={appDir + "/styles/globals.css"} />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
-
-export default MyApp;
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,13 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
+import { NextApiRequest, NextApiResponse } from 'next';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ name: 'John Doe' });
-}
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  return res.status(200).json({ name: 'John Doe' });
+};
+
+export default handler;
+
+// API routes support: https://nextjs.org/docs/api-routes/overview
+// Set API routes in pages/api/
+
+// Commit message:
+// "docs: update next.js handler function"
