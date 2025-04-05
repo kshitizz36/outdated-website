0ba5ac9ad7eafec2dec4700b35f0b719fdca2366
Kshitiz <kshitizyadav69@gmail.com>
2025-04-05 17:26:00 +0530

    Automated commit message.

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,13 +1,5 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextApiRequest, NextApiResponse } from 'next'
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  return res.status(200).json({ name: 'John Doe' });
-};
-
-export default handler;
-
-// API routes support: https://nextjs.org/docs/api-routes/overview
-// Set API routes in pages/api/
-
-// Commit message:
-// "docs: update next.js handler function"
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.status(200).json({ name: "John Doe" })
+}
